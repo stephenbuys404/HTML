d21ec530c02e67c599583a92e97e2a38147c9e81
Stephen Buys <stephenbuys404@gmail.com>
2022-09-21 16:16:11 +0200

    Upload Chapter11

---
+++ Chapter11/Array.js
@@ -0,0 +1,26 @@
+var people = ["Cellar", 
+"Flavor",
+"Cougar",
+"Chapter",
+"Mayor",
+"Anger",
+"Senator",
+"Passenger",
+"Major",
+"Popular",
+"Tractor",
+"Thunder",
+"Pillar",
+"Border",
+"Calendar",
+"Quarter",
+"Lunar",
+"Proper",
+"Elevator",
+"Bitter"];
+
+people = people.sort();
+
+
+
+document.getElementById("peopleIKnow").innerHTML = people.valueOf();
